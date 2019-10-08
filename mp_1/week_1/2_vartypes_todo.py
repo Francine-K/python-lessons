bb91496a5ebb859b25854714db90c757639340d2
Francine <francine.t.karpeh@gmail.com>
2019-10-07 21:07:48 -0400

    types and todos

---
+++ mp_1/week_1/2_vartypes_todo.py
@@ -0,0 +1,20 @@
+"""
+task 
+"""]]]
+# TODO: make a string and concatenate it with numbers, then print the result
+string1 = "Hello, "
+string2 = "my name is Franny "
+string3 = "369"
+
+print(string1 + string2 + string3)
+
+#TODO: set a float as a variable and then print that float
+float1 = 8.
+float2 = 8.3
+print(float2)
+
+# TODO: use the function type(variable-here) to print all the types we have worked with thus far
+print(type(100))
+print(type(string2))
+print(type(1.23456))
+print(type(True))
---
+++ mp_1/week_1/3_string_manipulationtodo.py
@@ -0,0 +1,23 @@
+"""
+Personality questionnaire
+"""
+# write a program that asks a user 6 unique questions about them and see if it works!
+# use input functions however you would like
+
+print("What is your favorite sport?")
+sport = input()
+animal = input("What is your favorite animal? ")
+sport_output = "Favorite sport: " + sport
+animal_output = "Favortie animal: " + animal
+
+print(sport_output)
+print(animal_output)
+
+color = input("What is your favorite color?")
+college_graduation = input("Where did you graduate college?")
+childhood_movie = input("What is your favorite childhood movie?")
+food = input("What is your favorite food?")
+tv_show = input("What is your favorite tv show?")
+city = input("What is your favorite city?")
+
+print("My favorite color is" + color + "I graduated college" +college_graduation+ "My favorite movie is" + childhood_movie + "My favorite food" +food+ "My favorite tv show is" +tv_show+ "My favorite city is" +city)
